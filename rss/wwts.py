7ba0a213efa30e890bc1d6511b78783f47ccd1bd
Dixie Flatline <umi0451@gmail.com>
2025-05-25 23:00:10 +0300

    Pools based on SQLite. Option to convert Pools instance of specific to some other type.

--- rss/wwts.py
+++ rss/wwts.py
@@ -163,6 +163,264 @@ class SQLProbCache:
 		probs = [(word, value) for word, value in c]
 		return probs
 
+class SQLPools:
+	def __init__(self, store_dir, dataClass=None):
+		self.dataClass = dataClass
+		self.store_dir = app.get_cache_dir() # TODO temp. during debugging. Should be: `store_dir`
+
+		self.in_transaction = False
+		self.conn = None
+		self.cache = SQLProbCache(dataClass=self.dataClass)
+
+		self.load()
+	def __del__(self): # TODO should be done explicitly, e.g. with help of context manager.
+		self.conn.close()
+	def lock(self):
+		if self.in_transaction:
+			return
+		self.in_transaction = True
+		self.conn.execute("""BEGIN;""")
+	def unlock(self):
+		if not self.in_transaction:
+			return
+		self.in_transaction = False
+		self.conn.commit()
+	def _commit(self):
+		if self.in_transaction:
+			return
+		self.conn.commit()
+
+	def import_from(self, other, tracer=None):
+		tracer = tracer or (lambda _:None)
+		for name in self.poolNames():
+			tracer('Removing pool {0}...'.format(repr(name)))
+			self.removePool(name)
+		for name in other.poolNames():
+			tracer('Recreating pool {0}...'.format(repr(name)))
+			self.newPool(name)
+		for name in self.poolNames() + ['__Corpus__']:
+			tracer('Repopulating pool {0}...'.format(repr(name)))
+			for index, (token, count) in enumerate(other.iter_pool_words(name)):
+				tracer('Repopulating pool {0} ({2}): {1}'.format(name, repr(token), index))
+				self.conn.execute("""
+					 INSERT INTO Tokens(pool_id, token, count)
+					 VALUES ( (SELECT id FROM Pools WHERE name = ?), ?, ?)
+					 ;""", (name, token, count))
+		tracer('Commiting changes...')
+		self._commit()
+		tracer('Invalidating cache...')
+		self.cache.invalidate()
+		tracer('Imported successfully.')
+
+	def get_pool_tokenCount(self, name):
+		c = self.conn.cursor()
+		c.execute("""
+				SELECT SUM(count) FROM Pools, Tokens
+				WHERE Pools.id = Tokens.pool_id
+				AND Pools.name = ?
+				;""", (name,))
+		self.conn.commit()
+		count = [int(f) for f, in c]
+		return count[0] if count else 0
+	def iter_pool_words(self, pname):
+		c = self.conn.cursor()
+		c.execute("""
+				SELECT token, count
+				FROM Tokens
+				WHERE pool_id = (SELECT id FROM Pools WHERE name = ?)
+				ORDER BY 1
+				;""", (pname,))
+		self.conn.commit()
+		for word, thisCount in c:
+			yield word, thisCount
+	def iter_pool_corpus_words(self, pname):
+		c = self.conn.cursor()
+		c.execute("""
+				SELECT corpus.token, corpus.count, this.count
+				FROM Tokens corpus, Tokens this
+				WHERE corpus.pool_id = (SELECT id FROM Pools WHERE name = '__Corpus__')
+				AND this.pool_id = (SELECT id FROM Pools WHERE name = ?)
+				AND corpus.token = this.token
+				;""", (pname,))
+		self.conn.commit()
+		for word, totCount, thisCount in c:
+			yield word, totCount, thisCount
+	def pool_inc_token(self, pool_name, token):
+		self.conn.execute("""
+				 INSERT INTO Tokens(pool_id, token, count)
+				 VALUES ( (SELECT id FROM Pools WHERE name = ?), ?, 0)
+				 ON CONFLICT(pool_id, token) DO UPDATE SET
+				 count = count + 1
+				 ;""", (pool_name, token))
+		self._commit()
+		self.cache.invalidate()
+	def pool_dec_token(self, pool_name, token):
+		self.conn.execute("""
+				 UPDATE Tokens
+				 SET count = count - 1
+				 WHERE pool_id = (SELECT id FROM Pools WHERE name = ?)
+				 AND token = ?
+				 ;""", (pool_name, token))
+		self._commit()
+		self.cache.invalidate()
+	def has_pool(self, name):
+		c = self.conn.cursor()
+		c.execute("""
+				SELECT COUNT(*) FROM Pools
+				WHERE name = ?
+				;""", (name,))
+		self.conn.commit()
+		count = [int(f) for f, in c]
+		return bool(count[0]) if count else False
+
+	def newPool(self, poolName):
+		"""Create a new pool, without actually doing any
+		training.
+		"""
+		self.conn.execute("""
+				 INSERT INTO Pools(name) VALUES (?)
+				 ;""", (poolName,))
+		self._commit()
+		self.cache.invalidate() # not always true, but it's simple
+	def removePool(self, poolName):
+		self.conn.execute("""
+				 DELETE FROM Tokens WHERE pool_id = (
+					 SELECT id FROM Pools WHERE name = ?
+				 )
+				 ;""", (poolName,))
+		self.conn.execute("""
+				 DELETE FROM Pools WHERE name = ?
+				 ;""", (poolName,))
+		self._commit()
+		self.cache.invalidate()
+	def renamePool(self, poolName, newName):
+		self.conn.execute("""
+				 UPDATE Pools SET name = ? WHERE name = ?
+				 ;""", (newName, poolName))
+		self._commit()
+		self.cache.invalidate()
+	def mergePools(self, destPool, sourcePool):
+		"""Merge an existing pool into another.
+		The data from sourcePool is merged into destPool.
+		The arguments are the names of the pools to be merged.
+		The pool named sourcePool is left in tact and you may
+		want to call removePool() to get rid of it.
+		"""
+		self.conn.execute("""
+				 UPDATE Tokens destPool
+				 SET count = count + (
+					SELECT count FROM Tokens sourcePool, Pools
+					WHERE sourcePool.pool_id = Pools.id
+					AND Pools.name = ?
+					AND sourcePool.token = destPool.token
+					AND sourcePool.pool_id = destPool.pool_id
+				 )
+				 WHERE id = (SELECT id FROM Pools WHERE name = ?)
+				 ;""", (sourcePool, destPool))
+		self.conn.execute("""
+				 INSERT INTO Tokens(pool_id, token, count)
+				 SELECT destPool.pool_id, sourcePool.token, sourcePool.count
+				 FROM Tokens sourcePool LEFT JOIN Tokens destPool
+					 ON destPool.token = sourcePool.token
+					 AND sourcePool.pool_id = (SELECT id FROM Pools WHERE name = ?)
+					 AND destPool.pool_id = (SELECT id FROM Pools WHERE name = ?)
+				 WHERE destPool.token IS NULL
+				 ;""", (sourcePool, destPool))
+		self._commit()
+		self.cache.invalidate()
+	def save(self, fname='train.sqlite'):
+		pass # Saved automatically on commits.
+	def load(self, fname='train.sqlite'):
+		if '/' not in fname:
+			fname = os.path.join(self.store_dir, fname)
+
+		if self.conn:
+			self.conn.close()
+
+		self.conn = sqlite3.connect(fname, timeout=10, check_same_thread=False) # To allow multithreading access.
+		self.conn.text_factory = str # To prevent some dummy encoding bug.
+		self.conn.execute("""
+				CREATE TABLE IF NOT EXISTS Pools (
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					name TEXT
+				)
+				;""")
+		self.conn.execute("""
+				CREATE TABLE IF NOT EXISTS Tokens (
+					pool_id INTEGER,
+					token TEXT,
+					count INTEGER,
+					PRIMARY KEY (pool_id, token)
+				)
+				;""")
+		self.conn.execute("""
+				CREATE TRIGGER IF NOT EXISTS delete_tokens_with_zeroed_count
+				AFTER UPDATE ON Tokens
+				BEGIN
+					DELETE FROM Tokens
+					WHERE pool_id = NEW.pool_id AND token = NEW.token
+					AND NEW.count <= 0;
+				END
+				;""")
+		self.conn.commit()
+
+		if not self.has_pool('__Corpus__'):
+			self.newPool('__Corpus__')
+		# Should invalidate cache in general case,
+		# but let's hope it's called just once for a Bayes object,
+		# so cache validity will be addressed later anyway:
+		# re-create for pickle-based cache, re-use for SQL-based cache.
+		#self.cache.invalidate()
+	def poolNames(self):
+		"""Return a sorted list of Pool names.
+		Does not include the system pool '__Corpus__'.
+		"""
+		c = self.conn.cursor()
+		c.execute("""
+				SELECT name FROM Pools
+				WHERE name <> '__Corpus__'
+				;""")
+		self.conn.commit()
+		return sorted([f for f, in c])
+
+	def _buildCache(self):
+		""" merges corpora and computes probabilities
+		"""
+		if self.cache.valid():
+			return
+		self.cache.lock()
+		self.cache.clear()
+		self.lock()
+		for pname in self.poolNames():
+			poolCount = self.get_pool_tokenCount(pname)
+			themCount = max(self.get_pool_tokenCount('__Corpus__') - poolCount, 1)
+			for word, totCount, thisCount in self.iter_pool_corpus_words(pname):
+				otherCount = float(totCount) - thisCount
+
+				if not poolCount:
+					goodMetric = 1.0
+				else:
+					goodMetric = min(1.0, otherCount/poolCount)
+				badMetric = min(1.0, thisCount/themCount)
+				f = badMetric / (goodMetric + badMetric)
+
+				# PROBABILITY_THRESHOLD
+				if abs(f-0.5) >= 0.1 :
+					# GOOD_PROB, BAD_PROB
+					self.cache.set_value(pname, word, max(0.0001, min(0.9999, f)))
+		self.unlock()
+		self.cache.unlock()
+	def guessProbs(self, tokens, combiner):
+		self._buildCache()
+		res = {}
+		for pname in self.cache.getNames():
+			p = self.cache.getProbs(pname, tokens)
+			if p:
+				res[pname] = combiner(p, pname)
+		res = list(res.items())
+		res.sort(key=lambda x: x[1], reverse=True)
+		return res
+
 class Pools:
 	def __init__(self, store_dir, dataClass=None):
 		if dataClass is None:
@@ -174,9 +432,14 @@ class Pools:
 		self.corpus = self.dataClass('__Corpus__')
 		self.pools['__Corpus__'] = self.corpus
 		self.cache = SQLProbCache(dataClass=self.dataClass)
+	def lock(self): pass
+	def unlock(self): pass
 	def get_pool_tokenCount(self, name):
 		pool = self.pools.get(name)
 		return sum(pool.values())
+	def iter_pool_words(self, pname):
+		for word, count in sorted(self.pools[pname].items()):
+			yield word, count
 	def iter_pool_corpus_words(self, pname):
 		for word, totCount in self.pools['__Corpus__'].items():
 			# for every word in the copus
@@ -231,11 +494,15 @@ class Pools:
 				dp[tok] = count
 		self.cache.invalidate()
 	def save(self, fname='train.pkl'):
-		fp = open(os.path.join(self.store_dir, fname), 'wb')
+		if '/' not in fname:
+			fname = os.path.join(self.store_dir, fname)
+		fp = open(fname, 'wb')
 		pickle.dump(self.pools, fp)
 		fp.close()
 	def load(self, fname='train.pkl'):
-		fp = open(os.path.join(self.store_dir, fname), 'rb')
+		if '/' not in fname:
+			fname = os.path.join(self.store_dir, fname)
+		fp = open(fname, 'rb')
 		self.pools = pickle.load(fp)
 		fp.close()
 		# Should invalidate cache in general case,
@@ -315,16 +582,24 @@ class Bayes(object):
 		self.save()
 
 	def newPool(self, poolName):
+		self.pools.lock()
 		self.pools.newPool(poolName)
+		self.pools.unlock()
 
 	def removePool(self, poolName):
+		self.pools.lock()
 		self.pools.removePool(poolName)
+		self.pools.unlock()
 
 	def renamePool(self, poolName, newName):
+		self.pools.lock()
 		self.pools.renamePool( poolName, newName)
+		self.pools.unlock()
 
 	def mergePools(self, destPool, sourcePool):
+		self.pools.lock()
 		self.pools.mergePools(destPool, sourcePool)
+		self.pools.unlock()
 
 	def save(self, fname='train.pkl'):
 		self.pools.save()
@@ -368,15 +643,19 @@ class Bayes(object):
 
 	def _train(self, tokens, pool_name):
 		wc = 0
+		self.pools.lock()
 		for token in tokens:
 			self.pools.pool_inc_token(pool_name, token)
 			self.pools.pool_inc_token('__Corpus__', token)
 			wc += 1
+		self.pools.unlock()
 
 	def _untrain(self, tokens, pool_name):
+		self.pools.lock()
 		for token in tokens:
 			self.pools.pool_dec_token(pool_name, token)
 			self.pools.pool_dec_token('__Corpus__', token)
+		self.pools.unlock()
 
 	def guess(self, msg):
 		tokens = Set(self.getTokens(msg))
@@ -459,14 +738,67 @@ def parse_args(args=None):
 	parser.add_argument('-t', '--train', action='store_true', help='Training mode')
 	parser.add_argument('-u', '--untrain', action='store_true', help='Untraining mode')
 	parser.add_argument('-g', '--guess', action='store_true', help='Guessing mode')
+	parser.add_argument('-C', '--convert', help='Treat input file as a DB and convert it to the given file (of another type).')
 	parser.add_argument('--train-dir', help='Root directory for WWTS train files. Default is {0}.'.format(app.Config.TRAIN_ROOT_DIR))
 	args = parser.parse_args(args)
 	return args
+
+def guess_db_type(filename):
+	_, ext = os.path.splitext(filename)
+	if ext == '.pkl':
+		return Pools
+	elif ext == '.sqlite':
+		return SQLPools
+	else:
+		raise RuntimeError('Cannot guess DB type from file: {0}'.format(filename))
+
+def stdout_traceline(line):
+	sys.stdout.write(line)
+	sys.stdout.write('\r')
 
 def run_wwts(args):
 	config = app.Config(
 			TRAIN_ROOT_DIR=args.train_dir,
 			)
+
+	if args.convert:
+		print('Converting {0} -> {1}'.format(args.file[0], args.convert))
+		type_src = guess_db_type(args.file[0])
+		type_dest = guess_db_type(args.convert)
+
+		src_db = type_src(config.TRAIN_ROOT_DIR)
+		src_db.load(args.file[0])
+
+		dest_db = type_dest(config.TRAIN_ROOT_DIR)
+		dest_db.load(args.convert)
+
+		dest_db.import_from(src_db, tracer=stdout_traceline)
+		sys.stdout.write('\n') # To preserve currently displayed traceline.
+		dest_db.save(args.convert)
+
+		print('Verifying...')
+		src_pools = set(src_db.poolNames())
+		dest_pools = set(dest_db.poolNames())
+		if src_pools != dest_pools:
+			print('List of pools differs:')
+			print('  src : {0}'.format(src_pools))
+			print('  dest: {0}'.format(dest_pools))
+			return 1
+		for name in src_db.poolNames() + ['__Corpus__']:
+			src_iter = iter(src_db.iter_pool_words(name))
+			dest_iter = iter(dest_db.iter_pool_words(name))
+			while True:
+				src_data = next(src_iter, None)
+				dest_data = next(dest_iter, None)
+				if src_data is None and dest_data is None:
+					break
+				if src_data != dest_data:
+					print('Pool {0}: tokens differ:'.format(name))
+					print('  src : {0}'.format(src_data))
+					print('  dest: {0}'.format(dest_data))
+					return 1
+		print('Done.')
+		return
 
 	wwts = Bayes(config, tokenizer=Tokenizer(lower=True))
 	try:
